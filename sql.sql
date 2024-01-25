011efe0d6fc39195116b38246330716c40f40a81
Ruchita Chaudhari <123711885+Ruchitachaudhari16@users.noreply.github.com>
2024-01-25 12:10:21 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,2 +1,20 @@
 Create database EmployePayrollService;
 use  EmployePayrollService;
+
+CREATE TABLE employee_Payroll
+(
+EmployeeID int AUTO_INCREMENT primary key,
+EmployeeName varchar(255),
+PhoneNumber varchar(255),
+Address varchar(255),
+Department varchar(255),
+Gender char(1),
+BasicPay float,
+Deductions float,
+TaxablePay float,
+Tax float,
+NetPay float,
+StartDate Date,
+City varchar(255),
+Country varchar(255)
+);
